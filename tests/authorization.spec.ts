1ff1253249cfa647cc7ad68f90f1dc3f94774a19
Serx <anissimov92@gmail.com>
2025-07-07 20:40:02 +0300

    Link was fixed and other stuffs and comments was deleted

--- tests/authorization.spec.ts
+++ tests/authorization.spec.ts
@@ -2,21 +2,17 @@ import { test, expect } from '@playwright/test';
 
 
 test('Negative login test', async ({ page }) => {
-  await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
-  // Fill in the username and password fields
+  await page.goto('https://fe-delivery.tallinn-learning.ee/');
   await page.getByTestId('username-input').fill('username123');
-  await page.getByTestId('password-input').fill('username123');
+  await page.getByTestId('password-input').fill('password123');
   await expect (page.getByTestId('signIn-button')).toBeEnabled();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('Authorization error pop-up is displayed', async ({ page }) => {
-  await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
-  // Fill in the username and password fields
+  await page.goto('https://fe-delivery.tallinn-learning.ee/');
   await page.getByTestId('username-input').fill('username123');
-  await page.getByTestId('password-input').fill('username123');
+  await page.getByTestId('password-input').fill('password123');
   await page.getByTestId('signIn-button').click()
   await expect (page.getByTestId('authorizationError-popup')).toBeVisible();
 });
